b7f01bea41b9a03e3be9205bde35b4a203f89816
DeversGreenGV <reutskiy-evgeniy@mail.ru>
2023-01-25 21:55:06 +0300

    Save9

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
     $('.reviews__list').slick({
-        // autoplay: true,
-        // autoplaySpeed: 6000,
+        autoplay: true,
+        autoplaySpeed: 6000,
     });
     $(".button").click(function() { // ID откуда кливаем
+        $('html, body').animate({
+        scrollTop: $(".order-form").offset().top // класс объекта к которому приезжаем
+        }, 1000); // Скорость прокрутки
+    });
+    $(".button").on("tap",function() { // ID откуда кливаем
         $('html, body').animate({
         scrollTop: $(".order-form").offset().top // класс объекта к которому приезжаем
         }, 1000); // Скорость прокрутки
