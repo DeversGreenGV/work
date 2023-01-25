7f80916d448740d6213510f39fa5941aa16cf8a4
DeversGreenGV <reutskiy-evgeniy@mail.ru>
2023-01-25 17:46:24 +0300

    Save1

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.reviews__list').slick({
+        autoplay: true,
+        autoplaySpeed: 6000,
+    });
+    $(".button").click(function() { // ID откуда кливаем
+        $('html, body').animate({
+        scrollTop: $(".order-form").offset().top // класс объекта к которому приезжаем
+        }, 1000); // Скорость прокрутки
+    });
+   
+});
